ec65c8f774d4e152f34c55bd463ae6db3671816f
picuu <carloscapoforcada@gmail.com>
2024-12-03 17:24:19 +0100

    add computed example with form

---
+++ src/components/ExampleComputed.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { computed, ref } from 'vue'
+
+const name = ref('Carlos')
+const surname = ref('Capó')
+
+const fullName = computed(() => `${name.value} ${surname.value}`)
+</script>
+
+<template>
+  <div>
+    <p>Nom complet: {{ fullName }}</p>
+    <input v-model="name" placeholder="Nom" />
+    <input v-model="surname" placeholder="Cognom" />
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,19 @@
 <script setup>
 import KeyForSample from '@/components/KeyForSample.vue'
 import WatchArrayExample from '@/components/WatchArrayExample.vue'
+import ExampleComputed from '@/components/ExampleComputed.vue'
 </script>
 
 <template>
   <section>
     <KeyForSample />
 
-    <br /><br />
+    <br /><br /><br />
 
     <WatchArrayExample />
+
+    <br /><br /><br />
+
+    <ExampleComputed />
   </section>
 </template>
