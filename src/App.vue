15e04c62ae7b312c894c2d0f338edf61a46f0e42
picuu <carloscapoforcada@gmail.com>
2024-12-05 19:33:11 +0100

    add vue transitions

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,22 @@ import HeaderApp from '@/components/HeaderApp.vue'
   <HeaderApp />
 
   <main class="container">
-    <RouterView />
+    <RouterView v-slot="{ Component }">
+      <Transition name="fade" mode="out-in">
+        <component :is="Component" />
+      </Transition>
+    </RouterView>
   </main>
 </template>
 
-<style scoped></style>
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
