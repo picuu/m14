9369e2f35901ccecf9b19424c581575dd4c6b85f
picuu <carloscapoforcada@gmail.com>
2025-02-04 17:15:49 +0100

    add pinia ex 3

--- src/components/CartWidget.vue
+++ src/components/CartWidget.vue
@@ -21,25 +21,19 @@ const active = ref(false);
       <div v-if="!cartStore.isEmpty">
         <ul class="items-in-cart">
           <CartItem
-            v-for="product in cartStore.items"
-            :key="product.name"
-            :product="{ name: 'Dried Pineapple', price: 5 }"
-            :count="5"
-            @updateCount=""
-            @clear=""
-          />
-          <CartItem
-            :product="{ name: 'Pineapple Gum', price: 3 }"
-            :count="5"
-            @updateCount=""
-            @clear=""
+            v-for="(items, name) in cartStore.grouped"
+            :key="name"
+            :product="items[0]"
+            :count="cartStore.groupCount(name)"
+            @updateCount="cartStore.setItemCount(items[0], $event)"
+            @clear="cartStore.clearItem(name)"
           />
         </ul>
         <div class="flex justify-end text-2xl mb-5">
-          Total: <strong>$40</strong>
+          Total: <strong>${{ cartStore.totalPrice }}</strong>
         </div>
         <div class="flex justify-end">
-          <AppButton class="secondary mr-2">Clear Cart</AppButton>
+          <AppButton class="secondary mr-2" @click="cartStore.$reset()">Clear Cart</AppButton>
           <AppButton class="primary">Checkout</AppButton>
         </div>
       </div>
--- src/stores/useCartStore.js
+++ src/stores/useCartStore.js
@@ -1,3 +1,4 @@
+import { groupBy, orderBy } from "lodash"
 import { defineStore } from "pinia"
 import { computed, ref } from "vue"
 
@@ -6,11 +7,33 @@ export const useCartStore = defineStore('CartStore', () => {
 
   const amount = computed(() => items.value.length)
   const isEmpty = computed(() => amount.value === 0)
-  const groupedItems = computed(() => Object.groupBy(items.value, (item) => item.name))
+  const grouped = computed(() => {
+    const orderedBy = orderBy(items.value, ['name'])
+    const groupedBy = groupBy(orderedBy, (item) => item.name)
+    return groupedBy
+  })
 
+  // Sumem les quantitats dels productes a l'accumulador, que comença en 0
+  const totalPrice = computed(() => items.value.reduce((acc, item) => acc += item.price, 0))
+  
+  const groupCount = (name) => grouped.value[name].length
+  
   const addItem = (count, item) => {
     for (let i = 0; i < count; i++) items.value.push({...item})
   }
 
-  return { items, amount, isEmpty, addItem }
+  const clearItem = (name) => {
+    items.value = items.value.filter((item) => item.name !== name)
+  }
+
+  const setItemCount = (item, count) => {
+    clearItem(item.name)
+    addItem(count, item)
+  }
+
+  const $reset = () => {
+    items.value = []
+  }
+
+  return { items, amount, isEmpty, grouped, totalPrice, groupCount, addItem, clearItem, setItemCount, $reset }
 })
