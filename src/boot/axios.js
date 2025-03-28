310989191ea358335e27c067d176f32276c863ca
picuu <carloscapoforcada@gmail.com>
2025-03-28 19:44:02 +0100

    refactor: use axios custom instance

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -7,7 +7,10 @@ import axios from 'axios'
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'https://api.example.com' })
+const api = axios.create({
+  baseURL: 'https://dummyapi.io/data/v1',
+  headers: { 'app-id': '662ce09b690e96c95e7103b4' },
+})
 
 export default defineBoot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
--- src/composables/useApi.js
+++ src/composables/useApi.js
@@ -1,16 +1,11 @@
-import axios from 'axios'
+import { api } from 'boot/axios'
 import { ref } from 'vue'
 
 const data = ref([])
 
 const useApi = () => {
-  const options = { headers: { 'app-id': '662ce09b690e96c95e7103b4' } }
-
   const getData = async (endpoint = '') => {
-    let validEndpoint = endpoint
-    if (endpoint.startsWith('/')) validEndpoint = endpoint.slice(1)
-
-    const res = await axios.get(`https://dummyapi.io/data/v1/${validEndpoint}`, options)
+    const res = await api.get(endpoint)
     const resData = res.data
     data.value = resData
   }
